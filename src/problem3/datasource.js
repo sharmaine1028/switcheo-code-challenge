8aea823b5394a0ecbe47625f4160fc07434c45fd
Sharmaine Teo <sharmaineteo@Shenyis-Mac-mini.local>
2023-04-01 21:11:31 +0800

    Delete unnecessary files for problem3

--- src/problem3/datasource.js
+++ src/problem3/datasource.js
@@ -1,30 +1,3 @@
-import express from "express";
-import request from "request";
-
-const app = express();
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
-app.get("/jokes/random", (req, res) => {
-  request(
-    { url: "https://cors-anywhere.herokuapp.com/" },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: "error", message: err.message });
-      }
-
-      res.json(JSON.parse(body));
-      console.log(res);
-    }
-  );
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
 class Datasource {
   constructor(url) {
     this.url = url;
